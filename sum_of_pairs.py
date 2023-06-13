4d1492bed617fc5b2e235c54442d8ce724b2d49e
amaurygelle <amaurygelle@gmail.com>
2023-06-14 00:15:25 +0200

    solved it

--- sum_of_pairs.py
+++ sum_of_pairs.py
@@ -9,20 +9,17 @@ the pair whose second element has the smallest index is the solution
 def sum_pairs(ints, s):
     index_1 = None
     index_2 = None
+    old_index_2 = 1000
     for i in range(len(ints)-1):
         j = 1
         while j < len(ints)-i:
             new_s = ints[i] + ints[i+j]
-            if new_s == s and index_2 and index_2 > i+j:
-                index_1 = i
-                print(index_1)
-                index_2 = i+j
-                print(index_2)
-            print(new_s)
+            if new_s == s:
+                index_1 = i if i+j < old_index_2 else index_1
+                index_2 = i+j if i+j < old_index_2 else index_2
+                old_index_2 = index_2
             j += 1
-    print('solution')
-    print(ints[index_1], ints[index_2])
-    return ints[index_1], ints[index_2] if index_1 and index_2 else None
+    return [ints[index_1], ints[index_2]] if index_1 != None and index_2 != None else None
 
 
 
@@ -30,4 +27,13 @@ if __name__ == '__main__':
     ints = [11, 3, 7, 5]
     s = 10
 
-    sum_pairs(ints, s)
+    ints = [4, 3, 2, 3, 4]
+    s = 6
+
+    ints = [4, 3, 2, 3, 4]
+    s = 2
+
+    ints = [20, -13, 40]
+    s = -7
+
+    print(sum_pairs(ints, s))
